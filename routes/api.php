30567bbaf0f951849e6c2a817c6b0a0bd3820a26
Paulo Veras <167385091+veraspvo@users.noreply.github.com>
2024-07-21 20:02:26 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,8 @@ use Illuminate\Support\Facades\Route;
 //    return response()->json(['status' => true, 'message' => 'Listar usuário',]);
 //});
 
-Route::get('/users', [UserController::class, 'index']);
+Route::get('/users', [UserApiController::class, 'index']); // GET - http;//1localhost:8000/api/users?page=1
+Route::post('/users', [UserApiController::class, 'store']); // POST - http;//1localhost:8000/api/users
+Route::delete('/users/{user}', [UserApiController::class, 'destroy']); // DELETE - http;//1localhost:8000/api/users/1
+Route::get('/users/{user}', [UserApiController::class, 'show']); // GET - http;//1localhost:8000/api/users/1
+Route::put('/users/{user}', [UserApiController::class, 'update']); // PUT - http;//1localhost:8000/api/users/1
